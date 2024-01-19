ba961751e086c1cf653dbc5e6ad155611babc64b
rahmsc <danmilad.code@gmail.com>
2024-01-19 12:29:27 +0700

    Created game & player schema. Added ponder gamecreation template

--- ponder/src/index.ts
+++ ponder/src/index.ts
@@ -1,12 +1,29 @@
 import { ponder } from "@/generated";
 
-ponder.on("Blitmap:Transfer", async ({ event, context }) => {
-  const { BlitmapToken } = context.db;
+ponder.on("R8R:GameCreated", async ({ event, context }) => {
+  const { Game } = context.db;
+  const { block, args } = event;
 
-  await BlitmapToken.create({
-    id: event.args.tokenId,
-    data: {
-      owner: event.args.to,
+  const gamesByNewest = await Game.findMany({
+    orderBy: {
+      id: "desc",
     },
   });
+  const newGameCount =
+    gamesByNewest[0] && gamesByNewest[0].nonce
+      ? gamesByNewest[0].nonce + BigInt(1)
+      : 1n;
+
+  await Game.create({
+    id: newGameCount.toString(),
+    data: {
+      status: true,
+      cost: args.cost,
+      prizePool: 0n,
+      createTimestamp: block.timestamp,
+      expireTimestamp: args.expireTimestamp,
+      nonce: newGameCount,
+    },
+  });
+  // Player on join game()
 });
