25d3addc0ca24ad717c0bd418f9beefa132709dc
rahmsc <danmilad.code@gmail.com>
2024-01-19 12:29:27 +0700

    Created game & player schema. Added ponder gamecreation template

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,8 +1,22 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  Example: p.createTable({
+  Game: p.createTable({
     id: p.string(),
-    name: p.string().optional(),
+    status: p.boolean(),
+    cost: p.bigint(),
+    prizePool: p.bigint(),
+    // players: p.many("Player.address"),
+    createTimestamp: p.bigint(),
+    expireTimestamp: p.bigint(),
+    nonce: p.bigint().references("Player.id"),
+  }),
+  Player: p.createTable({
+    id: p.string(),
+    address: p.string().references("Game.id"),
+    // gamesHistory: p.many("Game.nonce"),
+    gamesWon: p.bigint(),
+    gamesLost: p.bigint(),
+    winnings: p.bigint(),
   }),
 }));
